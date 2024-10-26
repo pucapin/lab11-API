7bf17f91a63598693f5868b97e5ca8478dd4e7dc
Isabella <isabellaguerrerorivera@gmail.com>
2024-10-26 18:18:49 -0500

    Buscador completo

--- app.js
+++ app.js
@@ -3,13 +3,16 @@
 
 class Agent {
 
-    constructor(name, background, image, description, roleDisplayName) {
+    constructor(name, background, image, description, roleDisplayName, abilitiesDisplay) {
         this.name = name;
         this.background = background;
         this.image = image;
         this.description = description;
         this.roleDisplayName = roleDisplayName;
+        this.abilitiesDisplay = abilitiesDisplay;
     }
+
+    
     htmlCard(pos) {
         return `
         <div id="agentcard">
@@ -21,10 +24,7 @@ class Agent {
                 <img src="${this.image}" alt="" id="img"> 
                 <div>
                 <h3>Abilities</h3>
-                <li>
-                    <ul>
-                    </ul>
-                </li>
+                     ${this.abilitiesDisplay.map(ability => `<li>${ability}</li>`).join('')}
                 </div>
                 </div>
             </div>
@@ -45,15 +45,21 @@ async function fetchAgents() {
     for (let i = 0; i < data.length; i++) {
         const agentJson = data[i];
         const roleDisplayName = agentJson.role ? agentJson.role.displayName : 'Unknown Role';
+        const abilitiesDisplay = agentJson.abilities 
+        ? agentJson.abilities.map(ability => ability.displayName) 
+        : [];
+
         const agent = new Agent(
             agentJson.displayName,
             agentJson.backgroundGradientColors[0],
             agentJson.displayIcon,
             agentJson.description,
-            roleDisplayName
+            roleDisplayName,
+            abilitiesDisplay,
         );
         agents.push(agent);
     }
+    console.log(agents)
     renderAgents()
 }
 
@@ -61,6 +67,30 @@ fetchAgents();
 
 function renderAgents() {
     const container = document.getElementById("container")
+
+    for(let i = 0; i < agents.length; i++) {
+        let agent = agents[i];
+        container.innerHTML += agent.htmlCard(i);
+    }
+
+}
+
+function filterAgents(event) {
+    const searchTerm = event.target.value.toLowerCase(); // get input !!
+
+    if (searchTerm === "") {
+        renderAgents();
+    } else {
+        const filteredAgents = agents.filter(agent => {
+            return agent.name.toLowerCase().includes(searchTerm);
+        });
+    
+        renderFilter(filteredAgents);
+    }
+}
+
+function renderFilter(agents) {
+    container.innerHTML = "";
 
     for(let i = 0; i < agents.length; i++) {
         let agent = agents[i];
